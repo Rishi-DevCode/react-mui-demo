54ac3b0add3d991c5ed1bab2190c5fd52efa393c
rishibhurane <bhuranerishi@gmail.com>
2023-05-12 09:35:39 +0530

    changes in the MuiTypography component & and problem facing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { MuiTypography } from './component/MuiTypography';
 
 function App() {
   return (
+    <>
     <div className="App">
-     
+     <MuiTypography></MuiTypography>
     </div>
+
+    </>
   );
 }
 
--- src/component/MuiTypography.tsx
+++ src/component/MuiTypography.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { Typography } from "@mui/material"
 
 export const MuiTypography = () => {
   return (
-    <div>MuiTypography</div>
+    <div>
+      <Typography variant="h1">h1 heading</Typography>
+      <Typography variant="h2">h2 heading</Typography>
+      <Typography variant="h3">h3 heading</Typography>
+      <Typography variant="h4">h4 heading</Typography>
+      <Typography variant="h5">h5 heading</Typography>
+      <Typography variant="h6">h6 heading</Typography>
+     
+      <Typography variant="subtitle1">Title 1</Typography>
+      <Typography variant="subtitle2">Title 2</Typography>
+     </div>
   )
 }
